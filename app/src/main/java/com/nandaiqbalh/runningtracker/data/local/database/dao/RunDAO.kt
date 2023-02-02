67395dc19260a746a3b2aee4788cdd8d4e76bc30
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2023-02-02 12:28:30 +0700

    RunEntity & RunDAO

---
+++ app/src/main/java/com/nandaiqbalh/runningtracker/data/local/database/dao/RunDAO.kt
@@ -0,0 +1,41 @@
+package com.nandaiqbalh.runningtracker.data.local.database.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.nandaiqbalh.runningtracker.data.local.database.entity.RunEntity
+
+@Dao
+interface RunDAO {
+	@Insert(onConflict = OnConflictStrategy.REPLACE)
+	suspend fun insertRun(run: RunEntity)
+
+	@Delete
+	suspend fun deleteRun(run: RunEntity)
+
+	@Query("SELECT * FROM running_table ORDER BY timestamp DESC")
+	fun getAllRunsSortedByDate(): LiveData<List<RunEntity>>
+
+	@Query("SELECT * FROM running_table ORDER BY timeInMillis DESC")
+	fun getAllRunsSortedByTimeInMillis(): LiveData<List<RunEntity>>
+
+	@Query("SELECT * FROM running_table ORDER BY caloriesBurned DESC")
+	fun getAllRunsSortedByCaloriesBurned(): LiveData<List<RunEntity>>
+
+	@Query("SELECT * FROM running_table ORDER BY avgSpeedInKMH DESC")
+	fun getAllRunsSortedByAvgSpeed(): LiveData<List<RunEntity>>
+
+	@Query("SELECT * FROM running_table ORDER BY distanceInMeters DESC")
+	fun getAllRunsSortedByDistance(): LiveData<List<RunEntity>>
+
+	@Query("SELECT SUM(timeInMillis) FROM running_table")
+	fun getTotalTimeInMillis(): LiveData<Long>
+
+	@Query("SELECT SUM(caloriesBurned) FROM running_table")
+	fun getTotalCaloriesBurned(): LiveData<Int>
+
+	@Query("SELECT SUM(distanceInMeters) FROM running_table")
+	fun getTotalDistance(): LiveData<Int>
+
+	@Query("SELECT AVG(avgSpeedInKMH) FROM running_table")
+	fun getTotalAvgSpeed(): LiveData<Float>
+}
--- app/src/main/java/com/nandaiqbalh/runningtracker/presentation/ui/home/MainActivity.kt
+++ app/src/main/java/com/nandaiqbalh/runningtracker/presentation/ui/home/MainActivity.kt
@@ -3,7 +3,9 @@ package com.nandaiqbalh.runningtracker.presentation.ui.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.nandaiqbalh.runningtracker.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
