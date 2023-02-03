53433f8b5b581198c173fd144471a37aaa97def7
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2023-02-03 10:18:46 +0700

    Handling map lifecycle

--- app/src/main/java/com/nandaiqbalh/runningtracker/presentation/ui/home/fragments/tracking/TrackingFragment.kt
+++ app/src/main/java/com/nandaiqbalh/runningtracker/presentation/ui/home/fragments/tracking/TrackingFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import com.google.android.gms.maps.GoogleMap
 import com.nandaiqbalh.runningtracker.databinding.FragmentTrackingBinding
 import com.nandaiqbalh.runningtracker.presentation.ui.home.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,14 +19,51 @@ class TrackingFragment : Fragment() {
 
 	private val viewModel: MainViewModel by activityViewModels()
 
+	private var map: GoogleMap? = null
+
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View? {
 		// Inflate the layout for this fragment
 		_binding = FragmentTrackingBinding.inflate(layoutInflater, container, false)
+		binding.mapView.onCreate(savedInstanceState)
+
+		binding.mapView.getMapAsync {
+			map = it
+		}
 
 		return binding.root
+	}
+
+	override fun onResume() {
+		super.onResume()
+		binding.mapView.onResume()
+	}
+
+	override fun onStart() {
+		super.onStart()
+		binding.mapView.onStart()
+	}
+
+	override fun onStop() {
+		super.onStop()
+		binding.mapView.onStop()
+	}
+
+	override fun onPause() {
+		super.onPause()
+		binding.mapView.onPause()
+	}
+
+	override fun onLowMemory() {
+		super.onLowMemory()
+		binding.mapView.onLowMemory()
+	}
+
+	override fun onSaveInstanceState(outState: Bundle) {
+		super.onSaveInstanceState(outState)
+		binding.mapView.onSaveInstanceState(outState)
 	}
 
 	override fun onDestroy() {
