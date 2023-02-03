f9646b27e2f835f993544d881315641bfff69f86
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2023-02-03 09:56:33 +0700

    Handling request permission

--- app/src/main/java/com/nandaiqbalh/runningtracker/presentation/ui/home/fragments/setup/SetupFragment.kt
+++ app/src/main/java/com/nandaiqbalh/runningtracker/presentation/ui/home/fragments/setup/SetupFragment.kt
@@ -1,5 +1,7 @@
 package com.nandaiqbalh.runningtracker.presentation.ui.home.fragments.setup
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,10 +10,14 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.nandaiqbalh.runningtracker.R
 import com.nandaiqbalh.runningtracker.databinding.FragmentSetupBinding
+import com.nandaiqbalh.runningtracker.other.Constants
+import com.nandaiqbalh.runningtracker.other.TrackingUtility
 import dagger.hilt.android.AndroidEntryPoint
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 @AndroidEntryPoint
-class SetupFragment : Fragment() {
+class SetupFragment : Fragment(), EasyPermissions.PermissionCallbacks {
 
 	private var _binding: FragmentSetupBinding? = null
 	private val binding get() = _binding!!
@@ -23,12 +29,72 @@ class SetupFragment : Fragment() {
 		// Inflate the layout for this fragment
 		_binding = FragmentSetupBinding.inflate(layoutInflater, container, false)
 
-		toRunFragment()
+		onClickListener()
+
+		requestPermission()
 
 		return binding.root
 	}
 
-	private fun toRunFragment(){
+
+	private fun requestPermission(){
+
+		// if the user already accept permission, just simply return it
+		if (TrackingUtility.hasLocationPermission(requireContext())){
+			return
+		}
+
+		// request permission (check version code first)
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q){
+			EasyPermissions.requestPermissions(
+				this,
+				getString(R.string.tv_rationale_permission),
+				Constants.REQUEST_CODE_LOCATION_PERMISSION,
+				Manifest.permission.ACCESS_FINE_LOCATION,
+				Manifest.permission.ACCESS_COARSE_LOCATION
+			)
+		} else{
+			EasyPermissions.requestPermissions(
+				this,
+				getString(R.string.tv_rationale_permission),
+				Constants.REQUEST_CODE_LOCATION_PERMISSION,
+				Manifest.permission.ACCESS_FINE_LOCATION,
+				Manifest.permission.ACCESS_COARSE_LOCATION,
+				Manifest.permission.ACCESS_BACKGROUND_LOCATION
+			)
+		}
+
+	}
+
+	override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+		TODO("Not yet implemented")
+	}
+
+	override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+
+		// if the user permanently denied the permission, then show dialog again
+		// -> because this app really need this permission
+		if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)){
+			AppSettingsDialog.Builder(this).build().show()
+		} else {
+			requestPermission()
+		}
+	}
+
+	// redirect the result from android framework to easy permission
+	// -> sho that easy permission know the result was
+	override fun onRequestPermissionsResult(
+		requestCode: Int,
+		permissions: Array<out String>,
+		grantResults: IntArray
+	) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+		// redirect the result
+		EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+	}
+
+	private fun onClickListener(){
 		binding.btnNext.setOnClickListener {
 
 			findNavController().navigate(R.id.action_setupFragment_to_runFragment)
