aea16e764801de7a1cdff42390e1d55d98ec18cb
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2023-02-04 11:33:43 +0700

    Drawing the running track

--- app/src/main/java/com/nandaiqbalh/runningtracker/presentation/ui/home/fragments/tracking/TrackingFragment.kt
+++ app/src/main/java/com/nandaiqbalh/runningtracker/presentation/ui/home/fragments/tracking/TrackingFragment.kt
@@ -7,10 +7,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.PolylineOptions
+import com.nandaiqbalh.runningtracker.R
 import com.nandaiqbalh.runningtracker.databinding.FragmentTrackingBinding
+import com.nandaiqbalh.runningtracker.other.Constants.ACTION_PAUSE_SERVICE
 import com.nandaiqbalh.runningtracker.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.nandaiqbalh.runningtracker.other.Constants.MAP_ZOOM
+import com.nandaiqbalh.runningtracker.other.Constants.POLYLINE_COLOR
+import com.nandaiqbalh.runningtracker.other.Constants.POLYLINE_WIDTH
 import com.nandaiqbalh.runningtracker.presentation.ui.home.viewmodel.MainViewModel
+import com.nandaiqbalh.runningtracker.services.Polyline
 import com.nandaiqbalh.runningtracker.services.TrackingService
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,6 +33,9 @@ class TrackingFragment : Fragment() {
 
 	private var map: GoogleMap? = null
 
+	private var isTracking = false
+	private var pathPoints = mutableListOf<Polyline>()
+
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
@@ -34,13 +46,83 @@ class TrackingFragment : Fragment() {
 
 		binding.mapView.getMapAsync {
 			map = it
+			addAllPolylines()
+
 		}
 
 		binding.btnToggleRun.setOnClickListener {
 			sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+			toggleRun()
 		}
 
+		subscribeToObservers()
+
 		return binding.root
+	}
+
+	private fun subscribeToObservers() {
+		TrackingService.isTracking.observe(viewLifecycleOwner, Observer {
+			updateTracking(it)
+		})
+
+		TrackingService.pathPoints.observe(viewLifecycleOwner, Observer {
+			pathPoints = it
+			addLatestPolyline()
+			moveCameraToUser()
+		})
+	}
+
+	private fun toggleRun() {
+		if(isTracking) {
+			sendCommandToService(ACTION_PAUSE_SERVICE)
+		} else {
+			sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+		}
+	}
+
+	private fun updateTracking(isTracking: Boolean) {
+		this.isTracking = isTracking
+		if(!isTracking) {
+			binding.btnToggleRun.setText(R.string.btn_start)
+			binding.btnFinishRun.visibility = View.VISIBLE
+		} else {
+			binding.btnToggleRun.setText(R.string.btn_stop)
+			binding.btnFinishRun.visibility = View.GONE
+		}
+	}
+
+	private fun moveCameraToUser() {
+		if(pathPoints.isNotEmpty() && pathPoints.last().isNotEmpty()) {
+			map?.animateCamera(
+				CameraUpdateFactory.newLatLngZoom(
+					pathPoints.last().last(),
+					MAP_ZOOM
+				)
+			)
+		}
+	}
+
+	private fun addAllPolylines() {
+		for(polyline in pathPoints) {
+			val polylineOptions = PolylineOptions()
+				.color(POLYLINE_COLOR)
+				.width(POLYLINE_WIDTH)
+				.addAll(polyline)
+			map?.addPolyline(polylineOptions)
+		}
+	}
+
+	private fun addLatestPolyline() {
+		if(pathPoints.isNotEmpty() && pathPoints.last().size > 1) {
+			val preLastLatLng = pathPoints.last()[pathPoints.last().size - 2]
+			val lastLatLng = pathPoints.last().last()
+			val polylineOptions = PolylineOptions()
+				.color(POLYLINE_COLOR)
+				.width(POLYLINE_WIDTH)
+				.add(preLastLatLng)
+				.add(lastLatLng)
+			map?.addPolyline(polylineOptions)
+		}
 	}
 
 	private fun sendCommandToService(action: String) =
--- app/src/main/java/com/nandaiqbalh/runningtracker/services/TrackingService.kt
+++ app/src/main/java/com/nandaiqbalh/runningtracker/services/TrackingService.kt
@@ -73,10 +73,14 @@ class TrackingService : LifecycleService() {
 						isFirstRun = false
 					} else {
 						Timber.d("Resuming service...")
+						startForegroundService()
+
 					}
 				}
 				ACTION_PAUSE_SERVICE -> {
 					Timber.d("Paused service")
+					pauseService()
+
 				}
 				ACTION_STOP_SERVICE -> {
 					Timber.d("Stopped service")
@@ -84,6 +88,10 @@ class TrackingService : LifecycleService() {
 			}
 		}
 		return super.onStartCommand(intent, flags, startId)
+	}
+
+	private fun pauseService() {
+		isTracking.postValue(false)
 	}
 
 	@SuppressLint("MissingPermission")
